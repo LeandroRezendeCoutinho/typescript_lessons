f3fc68426ebc9ff16a4ef206916b0684d23c3ca1
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2022-08-12 10:52:57 -0300

    type annotation examples

---
+++ src/type_annotation.ts
@@ -0,0 +1,48 @@
+
+// variable declaration
+function letDeclaration() {
+    let age: number = 39
+    let name: string = "Leandro"
+    let isUpdated: boolean = true
+
+    console.log(`My name is ${name}, i have ${age} years old. This info is ${isUpdated} `)
+}
+letDeclaration()
+
+// param declaration
+function param(id: number) {
+    console.log(id)
+}
+param(10)
+
+// object declaration
+var employee: {
+    id: number
+    name: string
+}
+
+employee = {
+    id: 100,
+    name: "leandro"
+}
+
+console.log(employee)
+
+function scoped() {
+    let num1: number = 10
+    let num2: number = 20
+    if (num1 < num2) {
+        let num3: number = 30
+        console.log(num1);
+        console.log(num2);
+        console.log(num3)
+    }
+    console.log(num1);
+    console.log(num2);
+    // console.log(num3)
+}
+
+scoped()
+
+const num: number = 10
+// num = 20
